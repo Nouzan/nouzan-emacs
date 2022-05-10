dcea1d4dc655ac2b5adb29fdc5f563b132ca00d8
Nouzan <clobraindie@outlook.com>
2022-05-10 18:47:27 +0000

    auto generated codes

--- standalone.el
+++ standalone.el
@@ -49,7 +49,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(exec-path-from-shell magit counsel rustic use-package)))
+ '(package-selected-packages
+   '(vscode-dark-plus-theme exec-path-from-shell magit counsel rustic use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
