76c3761ceb28bcf48fd94b61f613b38981054462
Nouzan <clobraindie@outlook.com>
2022-05-13 09:59:41 +0000

    auto

--- standalone.el
+++ standalone.el
@@ -50,7 +50,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(vscode-dark-plus-theme exec-path-from-shell magit counsel rustic use-package)))
+   '(key-chord expand-region ace-window vscode-dark-plus-theme exec-path-from-shell magit counsel rustic use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
